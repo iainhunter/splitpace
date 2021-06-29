ac0c7507978a7d7b495a7509ff9b95e0cde8cd28
Iain Hunter <iainhunter@Iains-MacBook-Pro.local>
2021-06-29 12:01:48 -0600

    Initial commit

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -4,8 +4,8 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
-
 import { Tab1PageRoutingModule } from './tab1-routing.module';
+
 
 @NgModule({
   imports: [
@@ -18,3 +18,4 @@ import { Tab1PageRoutingModule } from './tab1-routing.module';
   declarations: [Tab1Page]
 })
 export class Tab1PageModule {}
+
