1296b9a2fb5ab8d36ff97dce10a487123beb0a3d
Iain Hunter <iainhunter@Iains-MacBook-Pro.local>
2021-07-19 11:35:17 -0600

    ready to add split unit

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -33,11 +33,16 @@ export class Tab2Page {
   splitData = '';
   private subscription;
   public intervallTimer = interval(100);
+  public myTimer;
 
   ngOnInit() {
     this.predtimestring = this.convertsectoHMS(this.predtimeseconds);
     this.timedisplay = "0.00";
 
+    this.firstSplit = parseFloat((<HTMLInputElement>document.getElementById('firstSplit')).value);
+    if (isNaN(this.firstSplit)) {this.firstSplit = 400;}
+    this.splitDist = parseFloat((<HTMLInputElement>document.getElementById('splitDist')).value);
+    if (isNaN(this.splitDist)) {this.splitDist = 400;}
     //Calculate number of splits
     this.numSplits = (parseFloat(this.preddist)-this.firstSplit)/(this.splitDist)+1;
     this.finalSplit = Math.round((this.numSplits-Math.floor(this.numSplits)) * this.splitDist);
@@ -47,7 +52,7 @@ export class Tab2Page {
     
     this.speed = parseFloat(this.preddist)/parseFloat(this.predtimeseconds);
 
-    var splitText = ' Split&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Goal&nbsp;&nbsp;&nbsp;&nbsp;Actual&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Cumul';
+    var splitText = ' Split&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Goal&nbsp;&nbsp;&nbsp;&nbsp;Actual&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Lap';
     splitText = splitText + "<br>" + this.setToChars(this.firstSplit.toString(),5) + " " + this.setToChars(this.convertsectoHMS(this.firstSplit / this.speed),9);    
     for (let i = 1; i < this.numSplits; i++) {
       if (this.finalSplit && i==this.numSplits-1) {
@@ -110,21 +115,21 @@ export class Tab2Page {
     {
       this.lapsplit[this.completedSplits] = this.time - this.cumulsplit[this.completedSplits - 1];
     }
-    var splitText = ' Split&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Goal&nbsp;&nbsp;&nbsp;&nbsp;Actual&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Cumul';
+    var splitText = ' Split&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Goal&nbsp;&nbsp;&nbsp;&nbsp;Actual&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Lap';
     splitText = splitText + "<br>" + this.setToChars(this.firstSplit.toString(),5) + " " + this.setToChars(this.convertsectoHMS(this.firstSplit / this.speed),9);  
-    splitText = splitText + " " + this.setToChars(this.convertsectoHMS(this.lapsplit[1]),9) + " " + this.setToChars(this.convertsectoHMS(this.cumulsplit[1]),9);  
+    splitText = splitText + " " + this.setToChars(this.convertsectoHMS(this.cumulsplit[1]),9) + " " + this.setToChars(this.convertsectoHMS(this.lapsplit[1]),9);  
     for (let i = 1; i < this.numSplits; i++) {
       if (this.finalSplit && i==this.numSplits-1) {
         splitText = splitText + "<br>" + this.setToChars((this.firstSplit + (i-1) * this.splitDist + this.finalSplit),5) + " " + this.setToChars(this.convertsectoHMS(this.predtimeseconds),9);
         if (this.completedSplits > i) {
-          splitText = splitText + "  " + this.setToChars(this.convertsectoHMS(this.lapsplit[i+1]),9) + " " + this.setToChars(this.convertsectoHMS(this.cumulsplit[i+1]),9);
+          splitText = splitText + "  " + this.setToChars(this.convertsectoHMS(this.cumulsplit[i+1]),9) + " " + this.setToChars(this.convertsectoHMS(this.lapsplit[i+1]),9);
         }
       }
       else
       {
         splitText = splitText + "<br>" + this.setToChars((this.firstSplit) + i * this.splitDist,5) + " " + this.setToChars(this.convertsectoHMS((this.firstSplit + i * this.splitDist) / this.speed),9);
         if (this.completedSplits > i) {
-          splitText = splitText + "  " + this.setToChars(this.convertsectoHMS(this.lapsplit[i+1]),9) + " " + this.setToChars(this.convertsectoHMS(this.cumulsplit[i+1]),9);
+          splitText = splitText + "  " + this.setToChars(this.convertsectoHMS(this.cumulsplit[i+1]),9) + " " + this.setToChars(this.convertsectoHMS(this.lapsplit[i+1]),9);
         }
       }
 
@@ -135,10 +140,11 @@ export class Tab2Page {
     this.subscription = this.intervallTimer.subscribe(() => {
 
       if (this.startButton == "Finished") {
-        console.log("Finished: " + this.startButton);
           this.subscription.unsubscribe();
       }
 
+      else
+      {
       var n = new Date();
       this.time = (n.getTime() - this.starttime) / 1000;
 
@@ -172,14 +178,8 @@ export class Tab2Page {
         }
         this.timedisplay = hours + ':' + txtminutes + ':' + txtseconds;
       }
+    }
     });
-
-    console.log('compunit: ' + this.compunit);
-    console.log('timedata: ' + this.numtimedata);
-    console.log('preddist: ' + this.preddist);
-    console.log('compdist in meters: ' + this.compdistance);
-    console.log('compdist: ' + this.compdist);
-    console.log('Predrace: ' + this.predrace);
   }
 
   getDisplayTimer(time: number) {
